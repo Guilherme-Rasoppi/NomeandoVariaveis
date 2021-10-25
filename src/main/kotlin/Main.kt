6a5fa007a82ddb40a5e0f81fa1acd535e7067592
Guilherme Rasoppi <guilhermerasoppi@yahoo.com.br>
2021-10-25 13:18:30 -0300

    Nomeando variaveis

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var nome = "Guilherme"
+    var idade = 23
+    var anoNasc = idade - 2021
+    var prefEmprego = "Desenvolvedor mobile"
+    var nacionalidade = "Brasileiro"
+    var tipoSanguineo = "A"
+
+    println("Meu nome é $nome, eu tenho $idade de idade, minha preferência de emprego é " +
+            "$prefEmprego, minha nacionalidade é $nacionalidade, meu tipo sanguíneo é $tipoSanguineo" +
+            " e eu nasci no ano de $anoNasc")
+
+
+
+
+}
